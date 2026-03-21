acb58f182026a23bfb9a42a7c6bd1dfcfc9c270c
Althea <altheagraciellabagares@gmail.com>
2026-03-21 15:45:57 +0800

    fixed duplicated skiplist function in benchmark.cpp

--- Benchmarking/benchmark.cpp
+++ Benchmarking/benchmark.cpp
@@ -136,24 +136,6 @@ void benchmarkMeldableHeap() {
     }
 }
 
-void benchmarkSkiplist() {
-    for (int N = 1000; N <= 1000000; N += 10000) {
-        Skiplist<int> set; 
-        
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // Add N elements
-        for (int i = 0; i < N; ++i) {
-            set.add(i);
-        }
-        
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration = end - start;
-
-        std::cout << N << "," << duration.count() << "\n";
-    }
-}
-
 void benchmarkChainedHashSet() {
     for (int N = 1000; N <= 1000000; N += 10000) {
         ChainedHashSet<int> set; 
@@ -196,7 +178,6 @@ int main(int argc, char* argv[]) {
     else if (adt_choice == "Skiplist") {
         benchmarkSkiplist();
     }
-    
     else if (adt_choice == "ArrayDeque") {
         benchmarkArrayDeque();
     }        
